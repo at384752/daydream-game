1e64828914ec216d5262031d59eed5c5bcfe7321
at384752 <at384752@g.risd.org>
2025-10-05 12:31:34 -0500

    Re-export

--- transplant/scripts/main_menu.gd
+++ transplant/scripts/main_menu.gd
@@ -9,6 +9,3 @@ func _on_play_pressed() -> void:
 
 func _on_options_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/options.tscn")
-
-func _on_exit_pressed() -> void:
-	get_tree().quit()
