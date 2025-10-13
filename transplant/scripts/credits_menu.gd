565edf90b239d11d0e66bf3856ebc7d8b4a51ecc
at384752 <at384752@g.risd.org>
2025-10-13 01:01:37 -0500

    Update 25
    The final update before release
    (PCK file not included because it is too big)

--- transplant/scripts/credits_menu.gd
+++ transplant/scripts/credits_menu.gd
@@ -1,8 +1,16 @@
 extends Control
 
+var times_played = 0
+
 func _ready() -> void:
-	$AnimationPlayer/AnimatedSprite2D.play("default")
+	$AnimationPlayer/AnimatedSprite2D.play("walk")
 
 func _on_back_button_pressed() -> void:
 	AudioManager.ui_button.play()
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+
+
+func _on_button_pressed() -> void:
+	if times_played < 1:
+		$AnimationPlayer/AnimatedSprite2D.play("die")
+		times_played = times_played + 1
--- transplant/scripts/level_select.gd
+++ transplant/scripts/level_select.gd
@@ -7,20 +7,50 @@ func _ready() -> void:
 
 func _on_button_1_pressed() -> void:
 	AudioManager.ui_button.play()
+	preload_level_1()
 	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
 
 func _on_button_2_pressed() -> void:
 	AudioManager.ui_button.play()
+	preload_level_2()
 	get_tree().change_scene_to_file("res://scenes/level_2.tscn")
 
 func _on_button_3_pressed() -> void:
 	AudioManager.ui_button.play()
+	preload_level_3()
 	get_tree().change_scene_to_file("res://scenes/level_3.tscn")
 
 func _on_button_4_pressed() -> void:
 	AudioManager.ui_button.play()
+	preload_level_4()
 	get_tree().change_scene_to_file("res://scenes/level_4.tscn")
 
 func _on_back_button_pressed() -> void:
 	AudioManager.ui_button.play()
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+
+func preload_level_1() -> void:
+	preload("res://scenes/player_1.tscn")
+	preload("res://scenes/robot_1.tscn")
+	preload("res://scenes/button.tscn")
+	preload("res://scenes/door.tscn")
+	preload("res://scenes/trash.tscn")
+	preload("res://scenes/pause_menu.tscn")
+	preload("res://scenes/menu_button.tscn")
+	preload("res://scenes/level_complete.tscn")
+	preload("res://scenes/goal.tscn")
+
+func preload_level_2() -> void:
+	preload("res://scenes/player_2.tscn")
+	preload("res://scenes/robot_2.tscn")
+
+func preload_level_3() -> void:
+	preload("res://scenes/player_3.tscn")
+	preload("res://scenes/robot_3.tscn")
+	preload("res://scenes/lily_gate.tscn")
+	preload("res://scenes/rafflesia_gate.tscn")
+
+func preload_level_4() -> void:
+	preload("res://scenes/player_4.tscn")
+	preload("res://scenes/robot_4.tscn")
+	preload("res://scenes/gradient.tscn")
