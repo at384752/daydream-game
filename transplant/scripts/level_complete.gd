1a60736fe025158745846789613222f7805eaed5
at384752 <at384752@g.risd.org>
2025-10-13 12:43:47 -0500

    Update 26
    Bug fixes and optimizations

--- transplant/scripts/level_complete.gd
+++ transplant/scripts/level_complete.gd
@@ -20,10 +20,12 @@ func on_level_complete() -> void:
 func _on_level_select_button_pressed() -> void:
 	get_tree().paused = false
 	AudioManager.ui_button.play()
+	get_parent().queue_free()
 	get_tree().change_scene_to_file("res://scenes/level_select.tscn")
 
 
 func _on_main_menu_button_pressed() -> void:
 	get_tree().paused = false
 	AudioManager.ui_button.play()
+	get_parent().queue_free()
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
